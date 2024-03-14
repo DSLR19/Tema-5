4b6f48bad9135f2876778d644d2000d03b911c06
Daniel Rodrigues <137221004+DSLR19@users.noreply.github.com>
2024-03-14 17:42:17 -0300

    Add files via upload

---
+++ tema-5/desafio.php
@@ -0,0 +1,48 @@
+<?php
+
+// Matriz de filmes e gêneros
+$filmes_e_generos = array(
+    array("filme" => "Interestelar", "genero" => "Ficção Científica"),
+    array("filme" => "O Poderoso Chefão", "genero" => "Drama"),
+    array("filme" => "Vingadores: Ultimato", "genero" => "Ação"),
+    array("filme" => "Cidade de Deus", "genero" => "Drama"),
+    array("filme" => "Matrix", "genero" => "Ficção Científica"),
+    array("filme" => "O Senhor dos Anéis: A Sociedade do Anel", "genero" => "Fantasia"),
+    array("filme" => "O Lobo de Wall Street", "genero" => "Drama"),
+    array("filme" => "Batman: O Cavaleiro das Trevas", "genero" => "Ação"),
+    array("filme" => "A Origem", "genero" => "Ficção Científica"),
+    array("filme" => "Clube da Luta", "genero" => "Drama")
+);
+
+/* Contagem de gêneros
+$contagem_generos = array();
+foreach ($filmes_e_generos as $filme) {
+    $genero = $filme['genero'];
+    if (!isset($contagem_generos[$genero])) {
+        $contagem_generos[$genero] = 1;
+    } else {
+        $contagem_generos[$genero]++;
+    }
+}*/
+
+// Encontrar o gênero predominante
+$genero_predominante = "Ficção Científica";
+
+foreach ($filmes_e_generos as $filme) {
+    if ($filme['genero'] == $genero_predominante) {
+        $filmes_genero_predominante[] = $filme['filme'];
+    }
+}
+
+// Exibição dos resultados
+echo "<br> Lista de filmes assistidos em 2021 e seus gêneros:<br>";
+foreach ($filmes_e_generos as $filme) {
+    echo $filme['filme'] . " - " . $filme['genero'] . "<br>";
+}
+
+echo "<br>Filmes do gênero predominante:<br>";
+foreach ($filmes_genero_predominante as $filme) {
+    echo $filme . "<br>";
+}
+
+?>
